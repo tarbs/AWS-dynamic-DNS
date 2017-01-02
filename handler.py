205a4537bdd3eff59d24fff931a89847bfb9a091
Andy Tarbard <andy@tarbard.net>
2017-01-02 14:01:43 +0000

    Validation improvements

--- handler.py
+++ handler.py
@@ -1,20 +1,20 @@
-import json
 import boto3
 import os
 import re 
 
 def checkIP(event, context):
 
-    # TODO Add trailing '.' if missing
     # TODO ensure that function will create new recordset if required.
     # TODO add API key
     # TODO Validate all values
     # TODO add tests
 
     # Set up process variables
+    action_message = ""
     error = False
     error_message = ""
     source_ip = ""
+    update_response = ""
     source_ip_string = event["sourceip"]
     hosted_zone_name = os.environ["ZONE_NAME"]
     dns_address_string = os.environ["DNS_ADDRESS"]
@@ -29,7 +29,7 @@ def checkIP(event, context):
 
     # Validate the supplied DNS address
     if is_valid_hostname(dns_address_string):
-        DNSaddress = dns_address_string
+        DNSaddress = add_trailing_dot(dns_address_string)
     else:
         error = True
         error_message = "The supplied DNS address is not a valid domain name - the supplied value is : " + dns_address_string
@@ -42,31 +42,27 @@ def checkIP(event, context):
         error_message = "TTL value must be an integer - the supplied value is : " + dns_ttl_string
 
     if not error:
-
         r53_client = boto3.client('route53')
 
         r53_zone_response = r53_client.list_hosted_zones_by_name(DNSName=hosted_zone_name)
 
-        hosted_zone_id = r53_zone_response['HostedZones'][0]['Id'].split('/')[2]
+        if re.match('/hostedzone/.*', r53_zone_response['HostedZones'][0]['Id']) is not None:
+            hosted_zone_id = r53_zone_response['HostedZones'][0]['Id'].split('/')[2]
+        else:
+            hosted_zone_id = r53_zone_response['HostedZones'][0]['Id']
     
         r53_recordset_response = r53_client.list_resource_record_sets(HostedZoneId=hosted_zone_id, StartRecordName=DNSaddress)
     
-        found = "No"
         CurrentIPValue = ""
         for RRS in r53_recordset_response["ResourceRecordSets"]:
             if RRS["Name"] == dns_address_string:
-                found = "Yes"
                 for val in RRS["ResourceRecords"]:
                     CurrentIPValue = val["Value"]
     
-        
-        # TODO Validate that values are valid IPv4 addresses 
-    
-        updateRequired = "No"
-        updateResponse = ""
+
         if CurrentIPValue <> source_ip:
-            updateRequired = "Yes"
-            updateResponse = r53_client.change_resource_record_sets(
+            action_message = "IP addresses differ - requesting update to Route53 DNS"
+            update_response = r53_client.change_resource_record_sets(
                 HostedZoneId=hosted_zone_id,
                 ChangeBatch={
                     'Comment': 'Update to IP : ' + source_ip,
@@ -74,7 +70,7 @@ def checkIP(event, context):
                         {
                             'Action': 'UPSERT',
                             'ResourceRecordSet': {
-                                'Name': dns_address_string + '.',
+                                'Name': dns_address_string,
                                 'Type': 'A',
                                 'TTL': DNSTTL,
                                 'ResourceRecords': [
@@ -87,30 +83,37 @@ def checkIP(event, context):
                     ]
                 }
             )
-    
-            # TODO add error handling
-    
-    
+        else:
+            action_message = "IP addresses match - no update required"
+
         body = {
-            "message": "Received IP address",
+            "message": action_message,
             "clientIP": source_ip,
             "ZoneId": hosted_zone_id,
-            "currentIPset": CurrentIPValue,
-            "zone response": r53_zone_response,
-            "recordsets" : json.dumps(r53_recordset_response),
-            "found": found,
-            "updateRequired": updateRequired
+            "currentIPset": CurrentIPValue
         }
-    
     
         response = {
             "statusCode": 200,
-            "body": json.dumps(body),
-            "event": json.dumps(event)
+            "body": body,
+            "event": event
         }
         
         return response
+    else:
+        response = {
+            "statusCode": 400,
+            "errorMessage": error_message
+        }
+        return response
 
+
+def add_trailing_dot(input_string):
+    if len(input_string) > 0:
+        if input_string[-1] <> ".":
+            return input_string + "."
+    return input_string
+            
 
     # The following functions have been borrowed from StackOverflow to validate the supplied DNS name
 
